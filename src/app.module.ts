5b65e42db80801a659b5cd7107655efccf302135
Rafael Santos <rafael.bulaxa@gmail.com>
2023-06-21 16:53:25 -0300

    database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,25 @@ import { GptVoiceModule } from './modules/gpt_voice/gpt_voice.module';
 import { AuthModule } from './modules/auth/auth.module';
 import config from './config/config';
 import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [config],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: 3306,
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        syncronize: process.env.SYNCRONIZE,
+      }),
     }),
     GptTextModule,
     HttpModule,
